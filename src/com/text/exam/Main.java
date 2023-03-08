fac221237eb125f20623785411b77789a0e65b0f
Github <Github>
2023-03-08 22:48:53 +0900

    테스트 게시물 생성 함수 적용

--- src/com/text/exam/Main.java
+++ src/com/text/exam/Main.java
@@ -1,14 +1,25 @@
 package com.text.exam;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
+  static void makeTestData(ArrayList<Article> articles) {
+    articles.add(new Article(1,"제목1","내용1"));
+    articles.add(new Article(2,"제목1","내용1"));
+    articles.add(new Article(3,"제목1","내용1"));
+  }
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+
     System.out.println("==프로그램 시작 ==");
     int lastId = 1;
     Article lastArticle = null;
+    ArrayList<Article> articles = new ArrayList<Article>(); //<Article>이라는 클래스의 인스턴스 변수들을 가지고옴
+
+    makeTestData(articles);
+
 
     while (true) {
       System.out.printf("명령어 입력 : ");
@@ -17,7 +28,20 @@ public class Main {
 
       if (cmd.equals("나가기")) {
         break;
-      } else if (cmd.equals("게시물 작성")) {
+      }
+      else if (cmd.equals("게시물 리스트 보기")) {
+        System.out.println("==게시물 리스트==");
+        System.out.println("-----------------");
+        System.out.println("번호 / 제목");
+
+
+        for (Article article : articles) {
+          System.out.printf("%d / %s\n", article.lastId, article.title);
+        }
+        System.out.println("------------------");
+
+      }
+        else if (cmd.equals("게시물 작성")) {
         System.out.println("=========게시물=========");
         System.out.printf("제목 : ");
         String title = sc.nextLine();
@@ -39,7 +63,7 @@ public class Main {
           continue; // 밑에 내용(다음내용)으로 넘어가지고 않고 다시 while 반복문으로 올라감
         }
         Article article = lastArticle;        ///새로운 객체를 생성함으로써 class article의 인스턴스변수와 매소드의 정보를 이어서 넣어줌
-
+                                              ///이러면서 바로 전 내용의 게시물정보를 보여줌
         System.out.println("======마지막 게시물 상세보기=======");
         System.out.printf("제목 : %s\n",article.title);
         System.out.printf("내용 : %s\n",article.contents);
@@ -54,6 +78,8 @@ public class Main {
     System.out.println("=======프로그램 종료=========");
     sc.close();
   }
+
+
 }
 
 class Article {
@@ -69,6 +95,7 @@ class Article {
 
   @Override
   public String toString(){
+
     return String.format("제목 : \"%s\", 내용 : %s, ID :\" %s \"",title,contents,lastId);
   }
     }
